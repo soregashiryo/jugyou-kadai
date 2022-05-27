f1ead0a58356c024f0d1a858cd284383d0ac8d1f
soregashiryo <103712613+soregashiryo@users.noreply.github.com>
2022-05-27 12:05:09 +0900

    新規

--- jsadv/img/advmap.js
+++ jsadv/img/advmap.js
@@ -2,6 +2,30 @@ let showMap =() => {
     let elem = document.querySelector('div');
     let chip = map[y][x];
     elem.innerHTML = '<img src="img/' + images[chip] + '.png"><img scr="img/man.png">';
-    }
+};
 
+showMap();
+
+document.addEventListener('keyup',(event) => {
+     let keyname = event.key;
+        console.log(keyname);
+        let fname = images[map[y][x]];
+    if(keyname == 'ArrowLeft'){
+        if(fname.indexOf('left') > -1){
+    x -= 1;
+    }
+   } else if(keyname == 'ArrowRight'){
+     if(fname.indexOf('right') > -1){
+    x += 1;
+     }
+ }else if(keyname == 'ArrowUp'){
+if(fname.indexOf('up') > -1){
+    y -=1;
+ }
+}else if(keyname == 'ArrowDown'){
+     if(fname.indexOf('down') > -1){
+    y += 1;
+     }
+}
     showMap();
+} ) ;
