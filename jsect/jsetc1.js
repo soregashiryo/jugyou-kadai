d31cebf57a4e718483c6764ca4c545c36f04f49a
soregashiryo <103712613+soregashiryo@users.noreply.github.com>
2022-06-03 11:52:43 +0900

    修正

--- jsect/jsetc1.js
+++ jsect/jsetc1.js
@@ -3,5 +3,9 @@ let btn = document.querySelector('button');
 let elem = document.querySelector('p2');
 
 btn.addEventListener('click' ,() => {
-    elem.innerText = inp.value * 1.10;
+  if(isNaN(inp.value)){
+      elem.innerText = "<半角数字で数字を入力してください>";
+  } else {
+      elem.innerText = inp.value * 1.10;
+  }
 } );
