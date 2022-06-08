a68e1e91f8844658430929e69359d6c8eb6aaf39
soregashiryo <k19cb403@st.kyusan-u.ac.jp>
2022-06-09 00:34:36 +0900

    修正

--- jsect/jsetc2.js
+++ jsect/jsetc2.js
@@ -1,14 +1,14 @@
 let inp = document.querySelector('input');
 let btn = document.querySelector('button');
-let elem = document.querySelector('p2');
+let elem = document.querySelector('p');
 
 btn.addEventListener('click' ,() => {
     let age = parseInt(inp.value);
-  if( age < 20 ){
+    if( age < 20 ){
       elem.innerText = "あなたは未成年です。";
-  } else if( age >=65 ){
-      elem.innerText = "あなたは高齢者です。"
-  } else {
-      elem.innerText ="あなたは成人です。";
+  } else if( age >= 65 ){
+      elem.innerText = "あなたは高齢者です。";
+  } else{
+      elem.innerText = "あなたは成人です。";
   }
   } );
