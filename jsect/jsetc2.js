8045730584a6cd087bbd276efbe43a8caafcbf3e
soregashiryo <103712613+soregashiryo@users.noreply.github.com>
2022-06-10 11:56:36 +0900

    新規

--- jsect/jsetc2.js
+++ jsect/jsetc2.js
@@ -4,11 +4,20 @@ let elem = document.querySelector('p');
 
 btn.addEventListener('click' ,() => {
     let age = parseInt(inp.value);
-    if( age < 20 ){
-      elem.innerText = "あなたは未成年です。";
-  } else if( age >= 65 ){
+    if(isNaN(inp.value)){
+        elem.innerText = "<半角数字で数字を入れてください。";
+        inp.value="";
+    }else {
+      if( age < 20 ){
+          if( age >= 6 && age <=15 ){
+              elem.innerText = "あなたは未成年（義務教育期間）です。";
+          } else {
+               elem.innerText = "あなたは未成年です。";
+          }
+    } else if( age >= 65 ){
       elem.innerText = "あなたは高齢者です。";
-  } else{
+    } else{
       elem.innerText = "あなたは成人です。";
+    }
   }
-  } );
+} );
