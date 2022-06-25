bd47c53b8b7a966903a2b763e3e9480d7aebb3e1
soregashiryo <k19cb403@st.kyusan-u.ac.jp>
2022-06-25 09:49:14 +0900

    修正

--- jstest/jstest.js
+++ jstest/jstest.js
@@ -1,8 +1,21 @@
-let member1 = ['A','B','C','D','E'];
-let member2 = ['A','B','C','D','E'];
-for(let m1 of member1){
-    member2.shift();
-    for(let m2 of member2){
-        console.log(m1 + 'vs' + m2);
-    }
+let team = ["A","B","C","D","E"];
+
+let html = "<tr><th></th>";
+for(let x=0; x<=4; x+=1){
+    html += '<th>' + team[x] + '</th>';
 }
+html += '</tr>';
+
+for(let y=0; y<=4; y+=1){
+        html += '<tr><th>' + team[y] + '</th>';
+        for(let x=0; x<=4; x+=1){
+            if(x <= y){
+                html += '<td>' + '</td>';
+            } else {
+            html += '<td>' + team[y] + 'vs' +team[x] + '</td>';
+        }
+    }
+    html += '</tr>';
+}
+let elem = document.querySelector('table');
+elem.innerHTML = html;
